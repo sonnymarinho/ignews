d8f2fbe1471e9157622d1fad6d9d0b38f66ed41c
Sonny <sonny.marinho@gmail.com>
2021-10-16 17:14:31 -0300

    :tada: first commit!

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <h1>Hello World</h1>
+      </div>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+    </>
+  );
+}
