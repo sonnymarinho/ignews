cd03d23b9265952327c736e48b62c72bb51cd628
Sonny <sonny.marinho@gmail.com>
2021-10-17 17:34:30 -0300

    :sparkles: feat: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,28 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
-      <div>
-        <h1>Hello World</h1>
-      </div>
       <Head>
-        <title>ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, Welcome</span>
+          <h1>
+            News about the <span>React</span> World.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 per month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
